6a29664b32a1af62294cc1bab6f1f9da836c349f
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-06-15 13:26:28 +0300

    initialized project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const express = require('express');
+const cors = require('cors');
+const logger = require('./logger');
+
+const { port } = require('./config');
+
+const { pets, medications, logs, prescriptions } = require('./routes/v1');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/v1/pets/', pets);
+app.use('/v1/meds/', medications);
+app.use('/v1/logs/', logs);
+app.use('/v1/prescriptions/', prescriptions);
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Server is running' });
+});
+
+app.all('*', (req, res) => {
+  logger.warn(`Page not found: ${req.url}`);
+  res.status(404).send({ error: 'Page not found' });
+});
+
+app.listen(port, () => logger.info(`Listening on port ${port}`));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
